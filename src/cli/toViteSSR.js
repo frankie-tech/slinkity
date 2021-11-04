df0d5bb08ba63cbcfd5d47906c3e68b0f75cb4b4
Holben888 <bholmesdev@gmail.com>
2021-11-04 17:22:21 -0400

    feat: apply renderer vite configs using vite plugins

--- src/cli/toViteSSR.js
+++ src/cli/toViteSSR.js
@@ -37,6 +37,7 @@ function gimmeCSSPlugin() {
  * @typedef ViteSSRParams
  * @property {import('../plugin').SlinkityConfigOptions['environment']} environment
  * @property {import('../plugin').SlinkityConfigOptions['dir']} dir
+ * @property {import('../main/defineConfig').UserSlinkityConfig} userSlinkityConfig
  * @param {ViteSSRParams}
  *
  * @typedef {{
@@ -52,11 +53,19 @@ function gimmeCSSPlugin() {
  *
  * @returns {ViteSSR} viteSSR
  */
-module.exports = async function toViteSSR({ environment, dir }) {
+module.exports = async function toViteSSR({ environment, dir, userSlinkityConfig }) {
+  /**
+   * @type {import('vite').Plugin[]}
+   */
+  const rendererPlugins = userSlinkityConfig.renderers.map((renderer) => ({
+    name: `@slinkity/renderer-${renderer.name}`,
+    config: renderer.viteConfig ?? {},
+  }))
+
   const generatedStyles = gimmeCSSPlugin()
   const ssrViteConfig = defineConfig({
     root: dir.output,
-    plugins: [generatedStyles.plugin],
+    plugins: [generatedStyles.plugin, ...rendererPlugins],
   })
 
   if (environment === 'dev') {
